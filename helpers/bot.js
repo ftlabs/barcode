ce289b3574cab1b7f0067099237fea379084d253
Lily2point0 <lily.madar@ft.com>
2019-01-22 17:30:43 +0000

    make Slack silent if no Slack token in env

--- helpers/bot.js
+++ helpers/bot.js
@@ -4,21 +4,30 @@ const Slack = require('node-slack-upload');
 const scheduler = require('./scheduler');
 const Utils = require('./utils');
 
-const token = Utils.processEnv('SLACK_TOKEN');
+const token = Utils.processEnv('SLACK_TOKEN', {default: null});
 // const barcode = require('./barcode');
 
-const bot = new Slack(token);
+let bot;
 let pollInterval = null;
 
 function init() {
+	if(token === null) {
+		return;
+	}
+
+	bot = new Slack(token);
 	sendMessage();
-	//Try to send a message on launch, and retry avery 15 minutes
+	// //Try to send a message on launch, and retry avery 15 minutes
 	pollInterval = setInterval(sendMessage, Utils.minutesToMs(15));
 }
 
 function sendMessage() {
 	if(scheduler.onSchedule()) {
-		const channel = Utils.processEnv('SLACK_CHANNEL');
+		const channel = Utils.processEnv('SLACK_CHANNEL', {default: null});
+		if(channel === null) {
+			return;
+		}
+		
 		const fileName = '96dc48e407313515856ea7770fd15781'; //TODO: getHash dynamically
 		//TODO: check file exists, otherwise generate file first.
 		//TODO: add dynamic title with date ?
