1566ca9c006185970b9dae8b80f29e7b80b29ac4
Tobias <tobias.fieldhouse@FT-MW5211.local>
2019-01-28 17:03:41 +0000

    added origami layout

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,15 @@
 var queryLink;
-var elements = [];
+var inputs = [];
+var radios = [];
 var inputIds = [
   'width',
   'height',
   'dateFrom',
   'timeFrom',
   'dateTo',
-  'timeTo',
+  'timeTo'
+];
+var radioNames = [
   'orientation',
   'fit',
   'order',
@@ -17,9 +20,13 @@ document.addEventListener("DOMContentLoaded", init);
 
 function init(){
   queryLink	= document.getElementById('queryBuilderLink');
-  elements = getElements(inputIds);
-  addElementListeners(elements);
-  updateUrlBuilderLink(elements);
+
+  inputs = getElements(inputIds);
+  addInputListeners(inputs);
+  radios = getElements(radioNames);
+  addRadioListeners(radios);
+
+  updateUrlBuilderLink(inputs, radios);
 }
 
 function getElements(ids){
@@ -28,30 +35,45 @@ function getElements(ids){
     var element = ids[i];
     if(element !== undefined){
       elements[element] = document.getElementById(element);
+
+      if(elements[element] === null){
+        elements[element] = document.getElementsByName(element);
+      }
     }
   }
   return elements;
 }
 
-function addElementListeners(elements){
-  for(var key in elements){
-    if(elements.hasOwnProperty(key)){
-      elements[key].addEventListener('change', updateUrlBuilderLink, false);
+function addInputListeners(inputs){
+  for(var key in inputs){
+    if(inputs.hasOwnProperty(key)){
+      inputs[key].addEventListener('change', updateUrlBuilderLink, false);
+    }
+  }
+}
+
+function addRadioListeners(radios){
+  for(var set in radios){
+    for(var key in radios[set]){
+      if(radios[set].hasOwnProperty(key)){
+        radios[set][key].addEventListener('change', updateUrlBuilderLink, false);
+      }
     }
   }
 }
 
 function updateUrlBuilderLink(){
-  var widthVal = elements.width.value;
-  var heightVal = elements.height.value;
-  var dateFromVal = elements.dateFrom.value;
-  var timeFromVal = elements.timeFrom.value + ':00';
-  var dateToVal = elements.dateTo.value;
-  var timeToVal = elements.timeTo.value + ':00';
-  var orientationVal = elements.orientation.options[elements.orientation.selectedIndex].value;
-  var fitVal = elements.fit.options[elements.fit.selectedIndex].value;
-  var orderVal = elements.order.options[elements.order.selectedIndex].value;
-  var sortVal = elements.sort.options[elements.sort.selectedIndex].value;
+  console.log('foo');
+  var widthVal = inputs.width.value;
+  var heightVal = inputs.height.value;
+  var dateFromVal = inputs.dateFrom.value;
+  var timeFromVal = inputs.timeFrom.value + ':00';
+  var dateToVal = inputs.dateTo.value;
+  var timeToVal = inputs.timeTo.value + ':00';
+  var orientationVal =  document.querySelector('input[name="orientation"]:checked').value;
+  var fitVal = document.querySelector('input[name="fit"]:checked').value;
+  var orderVal = document.querySelector('input[name="order"]:checked').value;
+  var sortVal = document.querySelector('input[name="sort"]:checked').value;
 
   var str = '/barcode?width=' + widthVal + '&height=' + heightVal + '&dateFrom=' + dateFromVal + '&timeFrom=' + timeFromVal  + '&dateTo=' + dateToVal + '&timeTo=' + timeToVal + '&orientation=' + orientationVal + '&fit=' + fitVal + '&order=' + orderVal + '&sort=' + sortVal;
 
